b41a200b546e00603c89999ea73c0061ebae7713
NJOKU OKECHUKWU VALENTINE <bonsoirval@gmail.com>
2020-07-16 15:51:44 +0100

    Updated config files changing driver from mysql to postgre

--- config.php
+++ config.php
@@ -20,6 +20,18 @@ define('DIR_LOGS', DIR_STORAGE . 'logs/');
 define('DIR_SESSION', DIR_STORAGE . 'session/');
 define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 
+
+// DB
+define('DB_DRIVER', 'postgre');
+//define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'ec2-18-235-109-97.compute-1.amazonaws.com');
+define('DB_USERNAME', 'jdnjfyuhhpyqou');
+define('DB_PASSWORD', '2614ff42aac19d0b5b7fdc3a5ef8e5c69a1ab8eeb81e8db57df898ab894ee147');
+define('DB_DATABASE', 'de61661Host');
+define('DB_PORT', '5432');
+define('DB_PREFIX', 'oc_');
+
+/*
 // DB
 define('DB_DRIVER', 'postgre');
 //define('DB_DRIVER', 'mysqli');
@@ -29,3 +41,33 @@ define('DB_PASSWORD', 'nuhTMX6z');
 define('DB_DATABASE', 'multicart');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
+*/
+
+/*
+Host
+    ec2-18-235-109-97.compute-1.amazonaws.com
+Database
+    de61661Host
+    ec2-18-235-109-97.compute-1.amazonaws.com
+Database
+    de61661elfs7mc
+User
+    jdnjfyuhhpyqou
+Port
+    5432
+Password
+    2614ff42aac19d0b5b7fdc3a5ef8e5c69a1ab8eeb81e8db57df898ab894ee147
+URI
+    postgres://jdnjfyuhhpyqou:2614ff42aac19d0b5b7fdc3a5ef8e5c69a1ab8eeb81e8db57df898ab894ee147@ec2-18-235-109-97.compute-1.amazonaws.com:5432/de61661elfs7mc
+Heroku CLI
+    heroku pg:psql postgresql-elliptical-94107 --app multimartelfs7mc
+User
+    jdnjfyuhhpyqou
+Port
+    5432
+Password
+    2614ff42aac19d0b5b7fdc3a5ef8e5c69a1ab8eeb81e8db57df898ab894ee147
+URI
+    postgres://jdnjfyuhhpyqou:2614ff42aac19d0b5b7fdc3a5ef8e5c69a1ab8eeb81e8db57df898ab894ee147@ec2-18-235-109-97.compute-1.amazonaws.com:5432/de61661elfs7mc
+Heroku CLI
+    heroku pg:psql postgresql-elliptical-94107 --app multimart*/
